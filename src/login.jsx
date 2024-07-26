4ef62bbf9d34ecfb2afc917c025c5620f53bd266
Mschlei-48 <estermakade@gmail.com>
2024-07-26 11:07:16 +0200

    changed-files

--- src/login.jsx
+++ src/login.jsx
@@ -4,87 +4,66 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios'
 
 
 function Login(props){
-    // const navigate=useNavigate()
+    const navigate=useNavigate()
     const [userName,setUserName]=useState('')
     const [passWord,setPassWord]=useState('')
-    // const NewPage=(()=>{
-    //     navigate('register')
-    // })
+    const [usernames,setUsernames]=useState([])
+    const [passwords,setPasswords]=useState([])
+    const NewPage=(()=>{
+        navigate('register')
+    })
 
-    // const findUserDetails=((username,password)=>{
-        
-    //     const UserRecord=props.records.find((record)=>record.username===username)
-    //     if(UserRecord){
-    //         if(UserRecord.password===password){
-    //             navigate('home')
-    //         }
-    //         else{
-    //             alert('Incorrect Password')
-    //         }
-    //     }
-    //     else{
-    //         alert('User does not exist,please sign up')
-    //     }
-    //   })
-    // const handleLogin=(()=>{
-    //     findUserDetails(userName,passWord)
-    // })
     // const email_icon=<FontAwesomeIcon icon="fa-solid fa-envelope" />
-    // const [emailIcon,setEmailIcon]=useState(faEnvelope)
-    // const [passIcon,setPassIcon]=useState(faLock)
+    const [emailIcon,setEmailIcon]=useState(faEnvelope)
+    const [passIcon,setPassIcon]=useState(faLock)
 
-    // Database code
-    // const handleDatabaseConnection=async (e) =>{
-    //     e.preventDefault();
-    //     try{
-    //     const response=await fetch ('http://localhost:5173/',{
-    //         method:'POST',
-    //         headers:{
-    //             'Content-Type':'application/json'
-    //         },
-    //         body: JSON.stringify({ username: userName, password: passWord })
-    //     });
-    //     console.log("We fetched successfully")
-    //     if(!response.ok){
-    //         console.log("We did not get a response")
-    //         const errorData=await response.json();
-    //         throw new Error(errorData.message || "something went wrong")
-    //     }
-    //     else{
-    //         console.log("We got a response")
-    //         console.log("This is the response:",response)
-    //         const data=await response.json();
-    //         console.log('Raw response:', response.text()); // Log the raw response body
-    //         console.log('Parsed JSON:', data);
-    //         console.log(data);
-    //         alert(data.message);
-    //     }
-    
-
-    // } catch(error){
-    //     console.log("We tried to fetch but got an error")
-    //     console.log("Error:",error);
-    //     alert(error)
-    // }
-    // };
+    // Database code(Get user details and checkif the entered information matches the one in the database, if it does then log the person in)
+    // https://codedamn.com/news/reactjs/axios-network-requests
+    const getUsers=async ()=>{
+    const email_pattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ 
+    if(email_pattern.test(userName)===true){ 
+        try{
+            const response=await axios.get('http://localhost:3001/users') 
+             
+            if(response.data.find((record)=>record.username===userName && record.password===passWord)){
+                navigate('home')
+            }
+            else if(response.data.find((record)=>record.username===userName && record.password!==passWord)){
+                alert("Incorrect password,please enter correct password")
+            }
+            else if(response.data.find((record)=>record.username!==userName)){
+                alert("User does not exist, please register below")
+            }
+                      
+        }
+        catch(error){
+            alert(error.message,"Failed to retrive users")
+        }
+    }
+    else{
+        alert("Invalid email. Please enter valid email address")
+    }
+     
+    };
 
     return(
         <div className='main-content'>
-                {/* <FontAwesomeIcon icon={emailIcon} id='email-icon'/> */}
-                <input placeholder="    Email address" className="login-input" type='email' onChange={(event)=>setUserName(event.target.value)}></input>
+                <FontAwesomeIcon icon={emailIcon} id='email-icon'/>
+                <input placeholder="    Email address" className="login-input" type='email' onChange={(event)=>{setUserName(event.target.value);setEmailIcon()}}></input>
                 <br></br>
                 <br></br>
-                {/* <FontAwesomeIcon icon={passIcon} id='password-icon'/> */}
-                <input placeholder="    Password" className="login-input" type='password' onChange={(event)=>{setPassWord(event.target.value)}}></input>
+                <FontAwesomeIcon icon={passIcon} id='password-icon'/>
+                <input placeholder="    Password" className="login-input" type='password' onChange={(event)=>{setPassWord(event.target.value);setPassIcon()}}></input>
                 <br></br>
                 <br></br>
-                <button id='submit-button' onClick={handleDatabaseConnection}>Login</button>
+                <button id='submit-button' onClick={getUsers}>Login</button>
             <br></br>
             <br></br>
-            <button id='register-button'>Not a user? Click here to register</button>
+            <button id='register-button' onClick={NewPage}>Not a user? Click here to register</button>
     </div>
     )
 }
--- src/registration.jsx
+++ src/registration.jsx
@@ -11,8 +11,12 @@ function Register(props){
     const [passWord,setPassWord]=useState('')
     const [confirmPassword,setConfirmPassword]=useState('')
     const navigate=useNavigate()
+    const [emailIcon,setEmailIcon]=useState(faEnvelope)
+    const [passIcon,setPassIcon]=useState(faLock)
 
     const addUser = async () => {
+      const email_pattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+      if(passWord!=="" && passWord===confirmPassword && email_pattern.test(useName)==true){
         try {
           await axios.post('http://localhost:3001/register', {
             "username":useName,
@@ -23,21 +27,31 @@ function Register(props){
         } catch (error) {
           alert('Error registering user:' + error.message); // Use error.message for better feedback
         }
+      }
+      else if(email_pattern.test(useName)===false){
+        alert("Enter valid username")
+      }
+      else if(passWord===''){
+        alert("Invalid password, please enter a valid password")
+      }
+      else if(passWord!==confirmPassword){
+        alert("The passwords do not match");
+      }
+      
       };
-
 
     return(
         <div className='register-main-content'>
-            {/* <FontAwesomeIcon icon={emailIcon} id='reg-email-icon'/> */}
-                <input className="register-input" type='email' placeholder="    Email address" onChange={(event)=>{setUseName(event.target.value)}}></input>
+            <FontAwesomeIcon icon={emailIcon} id='reg-email-icon'/>
+                <input className="register-input" type='email' placeholder="    Email address" onChange={(event)=>{setUseName(event.target.value);setEmailIcon('')}}></input>
                 <br></br>
                 <br></br>
-                {/* <FontAwesomeIcon icon={passIcon} id='reg-password-icon'/> */}
-                <input placeholder="    Password" className="register-input" type='password' onChange={(event)=>{setPassWord(event.target.value)}}></input>
+                <FontAwesomeIcon icon={passIcon} id='reg-password-icon'/>
+                <input placeholder="    Password" className="register-input" type='password' onChange={(event)=>{setPassWord(event.target.value);setPassIcon('')}}></input>
                 <br></br>
                 <br></br>
-                {/* <FontAwesomeIcon icon={passIcon} id='reg-password-icon2'/> */}
-                <input placeholder="    Confirm Password" className="register-input" type='password'></input>
+                <FontAwesomeIcon icon={passIcon} id='reg-password-icon2'/>
+                <input placeholder="    Confirm Password" className="register-input" type='password' onChange={(event)=>{setConfirmPassword(event.target.value);setPassIcon('')}}></input>
                 <br></br>
                 <br></br>
                 <button type='submit' id='register-submit-button' onClick={addUser}>Register</button>
